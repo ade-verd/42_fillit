721c61d7e33b43dee97fc8de20beb9bc17bf71d1
Onur OZKAYA <oozkaya@e3r13p1.42.fr>
2017-11-28 16:11:47 +0100

    modif test et read du main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: oozkaya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 10:47:11 by oozkaya           #+#    #+#             */
-/*   Updated: 2017/11/28 15:47:49 by oozkaya          ###   ########.fr       */
+/*   Updated: 2017/11/28 16:08:51 by oozkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -16,20 +16,26 @@
 int		main(int ac, char **av)
 {
 	int		fd;
-	char	buf[21];
+	int		ret;
+	char	buf[11];
 
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
-			ft_putstr("No such file.\n");
-		else
 		{
-			read(fd, buf, 21);
-			buf[20] = '\0';
-			printf("buf:\n%s\n", buf);
-			close(fd);
+			ft_putstr("No such file.\n");
+			return (1);
 		}
+		ret = read(fd, buf, 10);
+		while (ret)
+		{
+			buf[ret] = '\0';
+			ft_putstr(buf);
+			ret = read(fd, buf, 10);
+		}
+		if (close(fd) == -1)
+			return (1);
 	}
 	else
 		ft_putstr("error\n");
