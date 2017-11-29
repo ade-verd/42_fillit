dadae414f8652c05a6a3732724e75ea89d1583b0
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2017-11-29 15:00:00 +0100

    [ade-verd] check ok

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: oozkaya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 08:31:45 by oozkaya           #+#    #+#             */
-/*   Updated: 2017/11/29 12:15:10 by ade-verd         ###   ########.fr       */
+/*   Updated: 2017/11/29 13:58:23 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
+# include "libft.h"
 
 # include <stdio.h> //A SUPPRIMER
 
 # define BUFF_SIZE 1
 
-int		ft_check_all(char **tab);
 char	*ft_read_buff(int fd);
 char	**ft_splittetris(char *str);
+int		ft_check_all(char **tab);
 
 #endif
--- src/checker.c
+++ src/checker.c
@@ -6,11 +6,22 @@
 /*   By: oozkaya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 13:18:20 by oozkaya           #+#    #+#             */
-/*   Updated: 2017/11/29 13:18:27 by oozkaya          ###   ########.fr       */
+/*   Updated: 2017/11/29 14:54:52 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_check_onetm(char *str)
+/*
+** int	ft_checkall(char **tab)
+** Check validity of each Tetrominos with :
+**
+** ft_check_content : check size and content ('#', '.','\n'))
+** &&
+** ft_contiguous_places : check if TM has pieces contiguous by at least 1 side.
+*/
+
+#include "fillit.h"
+
+static int	ft_check_content(char *str)
 {
 	int		i;
 	int		hashtag;
@@ -36,6 +47,47 @@ static int	ft_check_onetm(char *str)
 	return (0);
 }
 
+static int	ft_nbsides_touched(char *str, int pos)
+{
+	int		nb_sides;
+	int		len;
+
+	nb_sides = 0;
+	len = ft_strlen(str);
+	if ((pos - 1) >= 0 && str[pos - 1] == '#')
+		nb_sides++;
+	if ((pos + 1) < len && str[pos + 1] == '#')
+		nb_sides++;
+	if ((pos - 5) >= 0 && str[pos - 5] == '#')
+		nb_sides++;
+	if ((pos + 5) < len && str[pos + 5] == '#')
+		nb_sides++;
+	return (nb_sides);
+}
+
+static int	ft_contiguous_pieces(char *str)
+{
+	int		i;
+	int		nb_sides;
+	int		twosides;
+
+	i = 0;
+	twosides = 0;
+	while (str[i])
+	{
+		if (str[i] == '#')
+		{
+			nb_sides = ft_nbsides_touched(str, i);
+			if (nb_sides == 2)
+				twosides = 1;
+			if (nb_sides == 0)
+				return (0);
+		}
+		i++;
+	}
+	return (twosides);
+}
+
 int			ft_check_all(char **tab)
 {
 	int		tm_nb;
@@ -43,7 +95,12 @@ int			ft_check_all(char **tab)
 	tm_nb = 0;
 	while (tab[tm_nb])
 	{
-		if (ft_check_onetm(tab[tm_nb]) == 0)
+	//	ft_putstr("--------\n");//test affichage
+	//	ft_putstr(tab[tm_nb]);//test affichage
+	//	ft_putstr("--------\n");//test affichage
+		if (ft_check_content(tab[tm_nb]) == 0)
+			return (0);
+		if (ft_contiguous_pieces(tab[tm_nb]) == 0)
 			return (0);
 		tm_nb++;
 	}
